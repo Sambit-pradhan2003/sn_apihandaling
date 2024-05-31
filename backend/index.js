948fab425615870ff0e573e68c114e6afe03afa5
Sambit-pradhan2003 <snsambitpradhan@gmail.com>
2024-06-01 00:22:26 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+
+
+const app=express();
+
+app.get('/api/product',(req,res)=>{
+    const product=[
+        {
+          "id": "1",
+          "name": "Electric Kettle",
+          "price": 29.99,
+          "image": "https://example.com/images/electric-kettle.jpg"
+        },
+        {
+          "id": "2",
+          "name": "Electric Toaster",
+          "price": 19.99,
+          "image": "https://example.com/images/electric-toaster.jpg"
+        },
+        {
+          "id": "3",
+          "name": "Electric Blender",
+          "price": 49.99,
+          "image": "https://example.com/images/electric-blender.jpg"
+        },
+        {
+          "id": "4",
+          "name": "Electric Iron",
+          "price": 25.99,
+          "image": "https://example.com/images/electric-iron.jpg"
+        },
+        {
+          "id": "5",
+          "name": "Electric Coffee Maker",
+          "price": 39.99,
+          "image": "https://example.com/images/electric-coffee-maker.jpg"
+        }
+      ]
+
+      if (req.query.search ) {
+        const filterproduct=product.filter(product=>product.name.includes(req.query.search))  
+        res.send(filterproduct) ;
+        return; 
+        }
+      console.log("HELLO start");
+      setTimeout(()=>{
+        console.log( "hello2")
+        res.send(product)
+      },5000)
+      
+})
+
+
+
+
+
+
+const port=process.env.PORT||3000;
+app.listen(port,()=>{
+    console.log(`server runningg at port${port}`)
+});
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from "axios"
+
+function App() {
+//  const [count,error,loading] =Customreactquery("/api/product")
+  const [count, setCount] = useState([]);
+  const [error, seterror] = useState(false);
+  const [loading, setloading] = useState(false);
+  const [search,setsearch]=useState("")
+
+  useEffect(()=>{
+    const controller=new AbortController();
+    ;(async()=>{
+      try {
+        setloading(true)
+        seterror(false)
+        const response=await axios.get("/api/product?search"+search,{
+          signal:controller.signal
+        })
+        console.log(response.data)
+        setCount(response.data)
+        setloading(false)
+      } catch (error1) {
+        if (axios.isCancel(error1 )) {
+          console.log("request cancelled",error1.message)
+          return
+        }
+        seterror(true)
+        setloading(false)
+      }
+    })()
+    //cleanup code
+    return()=>{controller.abort()}
+  },[search])
+
+  // if(error){
+  //   return(
+  //     <h1>something went wrong</h1>
+  //   )
+  // }
+  // if (loading) {
+  //   return(
+  //     <h1>loading.......</h1>
+  //   )
+    
+  // }
+
+  return (
+    <>
+      <h1>hello sn</h1>
+      <input
+      type='text'
+      placeholder='search'
+      value={search}
+      onChange={(e)=>{setsearch(e.target.value)}}
+      />
+      {loading &&(<h1>loading.......</h1>)}
+      {error&&(<h1>something went wrong</h1>)}
+      <h2><b>number of products:{count.length}</b></h2>
+    </>
+  )
+}
+
+export default App
+
+// const Customreactquery=(urlpath)=>{
+//   const [count, setCount] = useState([]);
+//   const [error, seterror] = useState(false);
+//   const [loading, setloading] = useState(false);
+
+//   useEffect(()=>{
+//     ;(async()=>{
+//       try {
+//         setloading(true)
+//         seterror(false)
+//         const response=await axios.get(urlpath)
+//         console.log(response.data)
+//         setCount(response.data)
+//         setloading(false)
+//       } catch (error) {
+//         seterror(true)
+//         setloading(false)
+//       }
+//     })()
+//   },[])
+
+//   return[count,error,loading];
+
+// }
+
